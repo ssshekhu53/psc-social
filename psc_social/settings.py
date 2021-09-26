9ff24d25dd2e021b4d04b14271796fd1fbc5fffc
Shashank Shekhar <ssshekhu53@gmail.com>
2021-09-26 22:49:02 +0530

    Added custom tag to set variable value and added provision for active nav links

--- psc_social/settings.py
+++ psc_social/settings.py
@@ -71,6 +71,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'custom_template_tags': 'psc_social.templatetags.custom_template_tags'
+            },
         },
     },
 ]
