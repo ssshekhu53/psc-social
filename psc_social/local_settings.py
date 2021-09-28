1ca2ded8221e539423b7158ef8caae800be426f1
Shashank Shekhar <ssshekhu53@gmail.com>
2021-09-28 20:39:30 +0530

    Deleted local settings

--- psc_social/local_settings.py
+++
@@ -1,9 +0,0 @@
-DEBUG = True
-
-
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_HOST_USER = 'b93108daa3b712'
-EMAIL_HOST_PASSWORD = '4fb0da7b8e4568'
-EMAIL_PORT = 2525
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
