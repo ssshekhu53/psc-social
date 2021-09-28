fc6fddf98ee5e4b16fb76f6c60e02bd3eb452f76
Shashank Shekhar <ssshekhu53@gmail.com>
2021-09-28 21:04:27 +0530

    changed settings to collectstatic on heroku

--- psc_social/settings.py
+++ psc_social/settings.py
@@ -128,10 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/statics/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "statics"),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email configurations
 
