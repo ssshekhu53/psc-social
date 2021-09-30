c5ad5554dde96cfbe04038bedde7f99afb01343b
Shashank Shekhar <ssshekhu53@gmail.com>
2021-09-30 20:43:45 +0530

    Disabled collectstatic after following facebook's comment

--- psc_social/settings.py
+++ psc_social/settings.py
@@ -128,12 +128,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/statics/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "statics"),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+DISABLE_COLLECTSTATIC = 1
 
 # Email configurations
 
