617f9d1396ced0bc082dbc2fcebad0075365c29f
Shashank Shekhar <ssshekhu53@gmail.com>
2021-10-03 23:23:14 +0530

    After collectstatic Added backend, created email verification feature (no session yet)

--- staticfiles/assets/js/login.js
+++ staticfiles/assets/js/login.js
@@ -11,49 +11,44 @@ $('#login-form').validate({
         error.appendTo( element.parent().parent().children('.input-error') );
     },
     submitHandler: function (form) {
-        // let fd=new FormData(form);
-        // let url=$(form).attr('action');
-        // $.ajax({
-        //     type: 'POST',
-        //     url: url,
-        //     data: fd,
-        //     cache: false,
-        //     contentType: false,
-        //     processData: false,
-        //     dataType: 'json',
-        //     beforeSend: () => {
-        //         $(form).find('input, textarea, button').attr('disabled', true);
-        //         $(form).find('button').text('Sending');
-        //         $('#form-messages .alert').hide().removeClass('alert-danger');
-        //     },
-        //     success: function(response) {
-        //         console.log(response);
-        //         $(form).find('input, textarea, button').attr('disabled', false);
-        //         $(form).find('button').text('Send');
-        //         if(response.code==200) {
-        //             Swal.fire({
-        //                 icon: 'success',
-        //                 title: 'Message Sent',
-        //             });
-        //             $(form).trigger('reset');
-        //         }
-        //         else {
-        //             Swal.fire({
-        //                 icon: 'error',
-        //                 title: 'Message not sent',
-        //                 text: 'Try after some time',
-        //             });
-        //         }
-        //
-        //     },
-        //     error: function(jqXHR, textStatus, errorThrown) {
-        //         console.log(textStatus+' '+jqXHR+' '+errorThrown);
-        //         $(form).find('input, textarea, button').attr('disabled', false);
-        //         $(form).find('button').text('Send');
-        //         $('#form-messages .alert').addClass('alert-danger').html('<p class="h5">We are facing litle issue. Comeback after a while.</p>').show();
-        //         setTimeout(() => { $('#form-messages .alert').fadeOut(1000, () => { $('#form-message .alert').removeClass('alert-danger'); }); }, 3000);
-        //     }
-        // });
+        let fd=new FormData(form);
+        let url=$(form).attr('action');
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: fd,
+            cache: false,
+            contentType: false,
+            processData: false,
+            dataType: 'json',
+            beforeSend: () => {
+                $(form).find('input, textarea, button').attr('disabled', true);
+                $(form).find('button[type="submit"]').text('Logging In');
+                $('#form-messages .alert').hide().removeClass('alert-danger');
+            },
+            success: function(response) {
+                console.log(response);
+                $(form).find('input, textarea, button').attr('disabled', false);
+                $(form).find('button[type="submit"]').text('Log In');
+                if(response.code==200) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Logged In Successfully',
+                    });
+                    $(form).trigger('reset');
+                }
+                else {
+                    $('#form-messages .alert').addClass('alert-danger').html(`<p class="h5">${response.message}</p>`).show();
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus+' '+jqXHR+' '+errorThrown);
+                $(form).find('input, textarea, button').attr('disabled', false);
+                $(form).find('button[type="submit"]').text('Log In');
+                $('#form-messages .alert').addClass('alert-danger').html('<p class="h5">We are facing litle issue. Comeback after a while.</p>').show();
+                setTimeout(() => { $('#form-messages .alert').fadeOut(1000, () => { $('#form-message .alert').removeClass('alert-danger'); }); }, 3000);
+            }
+        });
     }
 });
 
